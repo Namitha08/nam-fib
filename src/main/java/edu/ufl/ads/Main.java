0318b79ca8983768cb04070055e8cbbda8c7bd5b
Shivakumar N <Shivakumara_Narayanaswamy@intuit.com>
2016-11-15 21:22:16 +0530

    -Working max code

--- src/main/java/edu/ufl/ads/Main.java
+++ src/main/java/edu/ufl/ads/Main.java
@@ -3,7 +3,6 @@ package edu.ufl.ads;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -15,26 +14,35 @@ import java.util.regex.Pattern;
  */
 public class Main {
 
-    public static final String REG_EX = "#([^\\s]+) (\\d+)";
+    public static final String REG_EX = "#([^\\s]+)\\s+(\\d+)";
     private static final Pattern PATTERN = Pattern.compile(REG_EX);
 
     public static void main(String[] args) {
 
-        FibonacciHeap fibonacciHeap = new FibonacciHeap();
+        MaxFibonacciHeap maxFibonacciHeap = new MaxFibonacciHeap();
         Map<String, Node> hashMap = new HashMap<String, Node>();
         Map<Node, String> reverseHash = new HashMap<Node, String>();
-//
-//        Node enqueue = fibonacciHeap.enqueue(21);
+
+        if(args.length != 1){
+            System.out.println("Please provide absolute path of the file!!");
+            System.exit(1);
+        }
+
+//        Node enqueue = maxFibonacciHeap.enqueue(21);
 //        hashMap.put("node1", enqueue);
-//        hashMap.put("node2", fibonacciHeap.enqueue(12));
-//        hashMap.put("node3", fibonacciHeap.enqueue(111));
-//        hashMap.put("node4", fibonacciHeap.enqueue(14));
-//        hashMap.put("node5", fibonacciHeap.enqueue(105));
+//        hashMap.put("node2", maxFibonacciHeap.enqueue(12));
+//        hashMap.put("node3", maxFibonacciHeap.enqueue(111));
+//        hashMap.put("node4", maxFibonacciHeap.enqueue(14));
+//        hashMap.put("node5", maxFibonacciHeap.enqueue(105));
 //
-//        fibonacciHeap.decreaseKey(hashMap.get("node4"), 123);
+//        maxFibonacciHeap.increaseKey(hashMap.get("node4"), 123);
 
         try {
-            File file = new File("/Users/sn1/github/fibonacci-heap/src/main/resources/sample.txt");
+            File file = new File(args[0]);
+            if(!file.exists()){
+                System.out.println("File not found");
+                System.exit(1);
+            }
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
@@ -50,31 +58,34 @@ public class Main {
                         //try to find if node is already present
                         Node node = hashMap.get(hashTagName);
 
-                        if(node == null){ // if node is null insert into fibonacciHeap and hashMap
-                            Node newNode = fibonacciHeap.enqueue(value);
+                        if(node == null){ // if node is null insert into maxFibonacciHeap and hashMap
+                            Node newNode = maxFibonacciHeap.enqueue(value);
                             newNode.name = hashTagName;
                             hashMap.put(hashTagName, newNode);
                             reverseHash.put(newNode, hashTagName);
                         } else { //if node is already exists increase node value
                             double existingValue = node.mValue;
-                            fibonacciHeap.decreaseKey(node, (existingValue + value));
+                            maxFibonacciHeap.increaseKey(node, (existingValue + value));
                         }
                     }
-                } else if(line.equals("stop")){
+                } else if(line.equalsIgnoreCase("stop")){
                     System.exit(1);
                 } else {
-                    System.out.println("----------------------------");
-                    Map<String, Double> buffer = new TreeMap<String, Double>(); //sort decreasing order
+                    Map<String, Double> buffer = new TreeMap<String, Double>();
                     Long aLong = Long.parseLong(line);
+                    boolean oneExists = false;
                     for(Long i = 0L; i < aLong; i++) {
-                        Node node = fibonacciHeap.dequeueMax();
+                        Node node = maxFibonacciHeap.dequeueMax();
                         String hashTagName = reverseHash.get(node);
-                        System.out.println(hashTagName + " -->"+ node.getPriority());
+                        if(oneExists)
+                            System.out.print(",");
+                        System.out.print(hashTagName);
+                        oneExists = true;
                         buffer.put(hashTagName, node.getPriority());
                     }
-
+                    System.out.println("");
                     for (String key : buffer.keySet()){
-                        Node enqueuedNode = fibonacciHeap.enqueue(buffer.get(key));
+                        Node enqueuedNode = maxFibonacciHeap.enqueue(buffer.get(key));
                         hashMap.put(key, enqueuedNode);
                         reverseHash.put(enqueuedNode, key);
                     }
@@ -82,7 +93,7 @@ public class Main {
             }
             fileReader.close();
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
---
+++ src/main/java/edu/ufl/ads/MaxFibonacciHeap.java
@@ -0,0 +1,289 @@
+package edu.ufl.ads;
+
+import java.util.*;
+
+public class MaxFibonacciHeap {
+
+    private Node maxNode = null; //Maximum node of the heap.
+
+    /**
+     * Insert element to the heap
+     * @param value new node value
+     * @return Node which got created in heap
+     */
+    public Node enqueue(double value) {
+        Node result = new Node(value);
+        maxNode = mergeLists(maxNode, result); //add new node to the existing tree
+        return result;
+    }
+
+    /**
+     * Get maximum element of the heap
+     * @return maximum element of the heap
+     */
+    public Node max() {
+        if (maxNode == null)
+            throw new IllegalAccessError("Trying to access empty heap.");
+        return maxNode;
+    }
+
+    /**
+     * Dequeue's the max element of the heap
+     *
+     * @return max element of the heap
+     */
+    public Node dequeueMax() {
+        if (maxNode == null) //isHeap empty?
+            throw new IllegalAccessError("Heap is empty.");
+
+        Node maxElem = maxNode;
+
+        /*
+         * Otherwise, if it's not null, then we write the elements next to the
+         * min element around the min element to remove it, then arbitrarily
+         * reassign the min.
+         */
+        if (maxNode.mNext == maxNode) { // max element doesn't have siblings assign max to null
+            maxNode = null;
+        }
+        else { // assign random node to max
+            maxNode.mPrev.mNext = maxNode.mNext;
+            maxNode.mNext.mPrev = maxNode.mPrev;
+            maxNode = maxNode.mNext;
+        }
+
+        /* Next, clear the parent fields of all of the max element's children,
+         * since they're about to become roots.  Because the elements are
+         * stored in a circular list, the traversal is a bit complex.
+         */
+        if (maxElem.mChild != null) {
+            /* Keep track of the first visited node. */
+            Node curr = maxElem.mChild;
+            do {
+                curr.mParent = null;
+
+                /* Walk to the next node, then stop if this is the node we
+                 * started at.
+                 */
+                curr = curr.mNext;
+            } while (curr != maxElem.mChild);
+        }
+
+        /* Next, splice the children of the root node into the topmost list, 
+         * then set maxNode to point somewhere in that list.
+         */
+        maxNode = mergeLists(maxNode, maxElem.mChild);
+
+        /* If there are no entries left, we're done. */
+        if (maxNode == null) return maxElem;
+
+        /* Next, we need to coalsce all of the roots so that there is only one
+         * tree of each degree.  To track trees of each size, we allocate an
+         * ArrayList where the entry at position i is either null or the 
+         * unique tree of degree i.
+         */
+        List<Node> treeTable = new ArrayList<Node>();
+
+        /* We need to traverse the entire list, but since we're going to be
+         * messing around with it we have to be careful not to break our
+         * traversal order mid-stream.  One major challenge is how to detect
+         * whether we're visiting the same node twice.  To do this, we'll
+         * spent a bit of overhead adding all of the nodes to a list, and
+         * then will visit each element of this list in order.
+         */
+        List<Node> toVisit = new ArrayList<Node>();
+
+        /* To add everything, we'll iterate across the elements until we
+         * find the first element twice.  We check this by looping while the
+         * list is empty or while the current element isn't the first element
+         * of that list.
+         */
+        for (Node curr = maxNode; toVisit.isEmpty() || toVisit.get(0) != curr; curr = curr.mNext)
+            toVisit.add(curr);
+
+        /* Traverse this list and perform the appropriate unioning steps. */
+        for (Node curr: toVisit) {
+            /* Keep merging until a match arises. */
+            while (true) {
+                /* Ensure that the list is long enough to hold an element of this
+                 * degree.
+                 */
+                while (curr.mDegree >= treeTable.size())
+                    treeTable.add(null);
+
+                /* If nothing's here, we're can record that this tree has this size
+                 * and are done processing.
+                 */
+                if (treeTable.get(curr.mDegree) == null) {
+                    treeTable.set(curr.mDegree, curr);
+                    break;
+                }
+
+                /* Otherwise, merge with what's there. */
+                Node other = treeTable.get(curr.mDegree);
+                treeTable.set(curr.mDegree, null); // Clear the slot
+
+                /* Determine which of the two trees has the smaller root, storing
+                 * the two tree accordingly.
+                 */
+                Node max = (other.mValue >= curr.mValue)? other : curr;
+                Node min = (other.mValue >= curr.mValue)? curr  : other;
+
+                /* Break min out of the root list, then merge it into max's child
+                 * list.
+                 */
+                min.mNext.mPrev = min.mPrev;
+                min.mPrev.mNext = min.mNext;
+
+                /* Make it a singleton so that we can merge it. */
+                min.mNext = min.mPrev = min;
+                max.mChild = mergeLists(max.mChild, min);
+                
+                /* Reparent min appropriately. */
+                min.mParent = max;
+
+                /* Clear min's mark, since it can now lose another child. */
+                min.mIsMarked = false;
+
+                /* Increase max's degree; it now has another child. */
+                ++max.mDegree;
+
+                /* Continue merging this tree. */
+                curr = max;
+            }
+
+            /* Update the global min based on this node.  Note that we compare
+             * for <= instead of < here.  That's because if we just did a
+             * reparent operation that merged two different trees of equal
+             * priority, we need to make sure that the min pointer points to
+             * the root-level one.
+             */
+            if (curr.mValue >= maxNode.mValue) maxNode = curr;
+        }
+        return maxElem;
+    }
+
+    public void increaseKey(Node node, double newValue) {
+        if (newValue < node.mValue)
+            throw new IllegalArgumentException("New value exceeds old.");
+
+        decreaseKey(node, newValue);
+    }
+
+    public void delete(Node node) {
+        //Simple hack to remove the node, assign node value to max and deque max element
+        decreaseKey(node, Double.MAX_VALUE);
+        dequeueMax();
+    }
+
+
+    /**
+     * Merges two lists to single list and returns max node
+     * @param one first tree head node
+     * @param two second tree head node
+     * @return max node of the merged list
+     */
+    private static Node mergeLists(Node one, Node two) {
+
+        if (one == null && two == null) {
+            return null;
+        }
+        else if (one != null && two == null) {
+            return one;
+        }
+        else if (one == null && two != null) {
+            return two;
+        }
+        else {  // join two linked lists
+            Node oneNext = one.mNext;
+            one.mNext = two.mNext;
+            one.mNext.mPrev = one;
+            two.mNext = oneNext;
+            two.mNext.mPrev = two;
+
+            return one.mValue > two.mValue ? one : two; //return max node
+        }
+    }
+
+    /**
+     *
+     * @param node The node whose key should be decreased.
+     * @param priority The node's new priority.
+     */
+    private void decreaseKey(Node node, double priority) {
+        /* First, change the node's priority. */
+        node.mValue = priority;
+
+        /* If the node no longer has a higher priority than its parent, cut it.
+         * Note that this also means that if we try to run a delete operation
+         * that decreases the key to -infinity, it's guaranteed to cut the node
+         * from its parent.
+         */
+        if (node.mParent != null && node.mValue >= node.mParent.mValue)
+            cutNode(node);
+
+        /* If our new value is the new min, mark it as such.  Note that if we
+         * ended up decreasing the key in a way that ties the current minimum
+         * priority, this will change the min accordingly.
+         */
+        if (node.mValue >= maxNode.mValue)
+            maxNode = node;
+    }
+
+    /**
+     * Cuts a node from its parent.  If the parent was already marked, recursively
+     * cuts that node from its parent as well.
+     *
+     * @param node The node to cut from its parent.
+     */
+    private void cutNode(Node node) {
+        /* Begin by clearing the node's mark, since we just cut it. */
+        node.mIsMarked = false;
+
+        /* Base case: If the node has no parent, we're done. */
+        if (node.mParent == null) return;
+
+        /* Rewire the node's siblings around it, if it has any siblings. */
+        if (node.mNext != node) { // Has siblings
+            node.mNext.mPrev = node.mPrev;
+            node.mPrev.mNext = node.mNext;
+        }
+
+        /* If the node is the one identified by its parent as its child,
+         * we need to rewrite that pointer to point to some arbitrary other
+         * child.
+         */
+        if (node.mParent.mChild == node) {
+            /* If there are any other children, pick one of them arbitrarily. */
+            if (node.mNext != node) {
+                node.mParent.mChild = node.mNext;
+            }
+            /* Otherwise, there aren't any children left and we should clear the
+             * pointer and drop the node's degree.
+             */
+            else {
+                node.mParent.mChild = null;
+            }
+        }
+
+        /* Decrease the degree of the parent, since it just lost a child. */
+        --node.mParent.mDegree;
+
+        /* Splice this tree into the root list by converting it to a singleton
+         * and invoking the merge subroutine.
+         */
+        node.mPrev = node.mNext = node;
+        maxNode = mergeLists(maxNode, node);
+
+        /* Mark the parent and recursively cut it if it's already been
+         * marked.
+         */
+        if (node.mParent.mIsMarked)
+            cutNode(node.mParent);
+        else
+            node.mParent.mIsMarked = true;
+
+        /* Clear the relocated node's parent; it's now a root. */
+        node.mParent = null;
+    }
+}
